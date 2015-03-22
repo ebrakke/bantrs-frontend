843c90205d5f47a68628b99418fd665f8ab82ce5
Tyler Waltze <tylerwaltze@gmail.com>
2015-03-22 00:54:26 -0400

    Enable CORS

--- api/app.js
+++ api/app.js
@@ -15,6 +15,35 @@ var comment = require('./routes/comment');
 
 var app = express();
 
+
+
+
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
+
+
+
 // view engine setup
 
 // app.set('views', path.join(__dirname, '../frontend/src/views'));
@@ -34,6 +63,8 @@ app.use('/', routes);
 app.use('/user', user);
 app.use('/room', room);
 app.use('/comment', comment);
+
+
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
