80c7be3c7b243f3d77f59230f3bb6a4f962b7ada
Tyler Waltze <tylerwaltze@gmail.com>
2015-04-23 20:11:58 -0400

    Finish user model
    Ability to update a user.

--- frontend/src/js/services/User.js
+++ frontend/src/js/services/User.js
@@ -7,10 +7,28 @@ app.factory('User', function(config, $http) {
         angular.extend(this, data);
     };
 
+    var getProperties = function() {
+        return {
+            uid: this.uid || null,
+            username: this.username || null,
+            email: this.email || null
+        };
+    };
+
     User.prototype.create = function() {
         var user = this;
 
-        return $http.post(api, user).success(function(response) {
+        return $http.post(api, user.getProperties()).success(function(response) {
+            user.uid = response.data.uid;
+        }).error(function(response) {
+
+        });
+    };
+
+    User.prototype.save = function() {
+        var user = this;
+
+        return $http.post(api + '/me', user.getProperties()).success(function(response) {
             user.uid = response.data.uid;
         }).error(function(response) {
 
@@ -18,8 +36,8 @@ app.factory('User', function(config, $http) {
     };
 
     User.get = function(id) {
-        return $http.get(api + '/' + id).then(function(response) {
-            return new User(response.data.data);
+        return $http.get(api + '/' + id).success(function(response) {
+            return new User(response.data);
         });
     };
 
@@ -27,7 +45,7 @@ app.factory('User', function(config, $http) {
         var user = this;
         var url = api + '/' + user.username +  '/rooms';
 
-        return $http.get(url).then(function(response) {
+        return $http.get(url).success(function(response) {
             return response.data;
         });
     };
